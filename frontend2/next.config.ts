a4cc4a03dcc559fd4c40007ad6251d873277c86c
Me-V <gonemanarmy@gmail.com>
2025-08-25 21:44:45 +0530

    frontend stuff

--- frontend2/next.config.ts
+++
@@ -1,7 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-    outputFileTracingRoot: "C:\\Users\\Vasu sharma\\Desktop\\mern-advanced-auth\\frontend2",
-};
-
-export default nextConfig;
